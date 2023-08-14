f6d33974a4da35bba155be15aa5e3babb2705fab
Brice Cothran <bcothran@unca.edu>
2023-08-14 19:57:48 -0400

    asdf
    asdf

---
+++ AirplaneGame_Git/Intermediate/Source/AirplaneGame.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class AirplaneGame : ModuleRules
+{
+	public AirplaneGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
